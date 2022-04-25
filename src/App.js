8e78a134d34f59635bfdf45709f14d6139d0a700
bhuvan <bhuvansbhuvan467@gmail.com>
2022-04-25 15:03:23 +0530

    bhuvan

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
+    <>
+     <h1 className='rene'>RENEWABLE ENERGY SOURCES</h1>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+    <img width='300' height='300' src="https://th.bing.com/th/id/OIP.Z6UJ8ix9oPJbLa9THu5hfwAAAA?w=168&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7" alt="not found" />
+    <img width='300' height='300' src="https://th.bing.com/th/id/OIP.szXfdj9D8pNtXV1XbzbxKAHaE8?w=232&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7" alt="not found" />
+    <img width='300' height='300' src="https://th.bing.com/th/id/OIP.j_o_cMaNS4W-Hsn2REI5TwHaE8?w=238&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7" alt="not found" />
+    <div>
+    <h2>BHUVAN S
+         
+         </h2>
+         <h2>1SB19CS016</h2>
+         <h2>CSE 'A' section</h2>
+         </div>
     </div>
+    <br />
+
+    <div className="next">
+    <img width='300' height='300' src="https://th.bing.com/th/id/OIP.watu9dQhZsKkwCJZoRETywHaED?w=316&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7" alt="not found" />
+    <img width='300' height='300' src="https://th.bing.com/th/id/OIP.Zg_Ds6fZaOgQSys-4E-KqQHaHa?w=174&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7" alt="not found" />
+    <img width='300' height='300' src="https://th.bing.com/th?q=Hydropower+for+Kids&w=120&h=120&c=1&rs=1&qlt=90&cb=1&dpr=1.5&pid=InlineBlock&mkt=en-IN&cc=IN&setlang=en&adlt=moderate&t=1&mw=247" alt="not found" />
+    
+
+    </div>
+    <br />
+    <div className="last">
+    <img width='300' height='300' src="https://th.bing.com/th/id/OIP.S76xD8MQszs9B4_NPhoIEAHaHY?w=170&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7" alt="not found" />
+    <img width='300' height='300' src="https://th.bing.com/th/id/OIP.DWOdGkN9HNRlRo41QBdRCAHaDt?w=286&h=174&c=7&r=0&o=5&dpr=1.5&pid=1.7" alt="not found" />
+    <img width='300' height='300' src="https://th.bing.com/th/id/OIP.eVdvqng0thQXIgp1G9mN5gHaFx?w=238&h=185&c=7&r=0&o=5&dpr=1.5&pid=1.7" alt="not found" />
+
+    </div>
+    </>
   );
 }
 
